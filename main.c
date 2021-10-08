9e86f9ad82db3a4062a56ecf9ab444e8b7631d0e
lizziejuks <91128262+lizziejuks@users.noreply.github.com>
2021-10-08 14:22:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+----------------------------
+AUTHOR: ELIZABETH ATIENO
+PROJECT: LIBRARY MANAGEMENT SYSTEM
+LICENCE: MIT
+DATE: 08/10/2021
+-----------------------------
+*/
+#include<stdio.h> //inluding a library
+int main(){
+    printf("\t Zetech University Library System\n");
+    printf("1:Login \n");
+    printf("Welcome miss Elizabeth\n");
+    printf("2:Add a User \n");
+    printf("3:Add a book \n");
+    printf("4:Remove a book \n");
+    printf("5:Borrow a book \n");
+    printf("6:Delete user \n");
+    printf("7:Return a book \n");
+    printf("8:Logout\n");
+    return 0;
+}
